c1a59c19bf7731f89a6804b6898720ee210383dd
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-07-15 13:45:21 -0300

    Req 1 completo

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,21 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    mozzarella_cheese = Ingredient("queijo mussarela")
+    flour = Ingredient("farinha")
+
+    assert mozzarella_cheese == Ingredient("queijo mussarela")
+    assert isinstance(mozzarella_cheese, Ingredient)
+    assert mozzarella_cheese.name == "queijo mussarela"
+    assert hash(mozzarella_cheese) != hash(flour)
+    assert hash(mozzarella_cheese) == hash(mozzarella_cheese)
+    assert mozzarella_cheese.__repr__() == "Ingredient('queijo mussarela')"
+    assert mozzarella_cheese.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
